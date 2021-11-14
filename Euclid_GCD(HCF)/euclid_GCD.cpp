93c77e7ae319da9a316bfcf0f7d9be111f43e626
akhilelamadu <33170852+akhilelamadu@users.noreply.github.com>
2021-11-14 12:10:15 +0530

    codded

---
+++ Euclid_GCD(HCF)/euclid_GCD.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{   
+    int l=a,s=b;
+    if(a<b)
+    {
+        s=a;
+        l=b;
+    }
+
+    int temp;
+    while(s!=0)
+    {   
+        temp=s;
+        s=l%s;
+        l=temp;
+    }
+    return temp;
+}
+
+int main()
+{
+    cout<<gcd(20,8)<<"\n";
+    cout<<gcd(8,20);
+
+    return 0;
+
+}
