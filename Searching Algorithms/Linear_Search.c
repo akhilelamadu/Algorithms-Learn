c3b83f1438e218dce83bbbf08f9021a8f20aba65
akhilelamadu <33170852+akhilelamadu@users.noreply.github.com>
2022-02-13 15:22:47 +0530

    Completed the linear search code

--- Searching Algorithms/Linear_Search.c
+++ Searching Algorithms/Linear_Search.c
@@ -1,19 +1,53 @@
 #include <cstdio>
 #include <cstdlib>
+#include <ctime>
 
 int main()
-{
-    int i, n;
-    time_t t;
-   
-   n = 5;
-   
+{  
+   int i;
    /* Intializes random number generator */
-   srand((unsigned) time(&t));
+   time_t t;
+   struct tm *tm;
+   tm = localtime (&t);
+   srand((unsigned) time_t(&tm->tm_sec));
+   
+   /* Asking user for the length */
+   int n;
+   printf("Enter the length of the list");
+   scanf("%d", &n);
+   int A[n];
 
-   /* Print 5 random numbers from 0 to 10 */
-   for( i = 0 ; i < n ; i++ ) {
-      printf("%d\n", rand() % 10);
+   /* Print and store n random numbers from 0 to 10 */
+   printf("The list is :");
+   for(i = 0 ; i < n ; i++ )
+   {
+      A[i]=rand()% 10;
+      printf("%d ", A[i] );
    }
-   return 0
+
+   /* Searching */
+   int d, flag=0;
+   printf("\nEnter the search  digit\n");
+   scanf("%d",&d);
+
+   for(i=0;i<n;i++)
+   {
+      if(d==A[i])
+      {
+         flag =1;
+         break;
+      }
+   }
+
+   if(flag==0)
+   {
+      printf("No search found\n");
+   }
+   else
+   {
+      printf("The digit is located at %d position",++i);
+   }
+
+
+   return 0;
 }
